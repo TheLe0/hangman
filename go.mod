9bea03d15cb9fd6c5760389287fcdf2ecbedcabb
TheLe0 <leob.tosin@hotmail.com>
2021-05-27 21:46:16 -0300

    Main structure

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module main
 
 go 1.16
+
+require (
+	github.com/buger/goterm v1.0.0 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20200609044655-c4b36f998cf2 // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+)
--- src/main.go
+++ src/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/inancgumus/screen"
 )
 
 func randomWord() string {
@@ -23,13 +27,116 @@ func main() {
 
 	var word string
 	var input string
+	var lifes int
+	logo := figure.NewColorFigure("Hangman", "", "red", true)
+	win := figure.NewColorFigure("You Win!", "", "green", true)
+	lost := figure.NewColorFigure("You Died!", "", "red", true)
+	var stages [7]string
 
-	word = randomWord()
+	stages[0] = `
+			+---+
+			|   |
+			O   |
+		   /|\  |
+		   / \  |
+				|
+		=========
+	`
 
-	fmt.Println("Digit a letter: ")
-	fmt.Scanln(&input)
+	stages[1] = `
+			+---+
+			|   |
+			O   |
+		   /|\  |
+		   /    |
+				|
+		=========
+	`
 
-	for pos, char := range word {
-		fmt.Printf("character %c starts at byte position %d\n", char, pos)
+	stages[2] = `
+			+---+
+			|   |
+			O   |
+		   /|\  |
+			    |
+				|
+		=========
+	`
+
+	stages[3] = `
+			+---+
+			|   |
+			O   |
+		   /|  	|
+				|
+				|
+		=========
+	`
+
+	stages[4] = `
+			+---+
+			|   |
+			O   |
+		   /  	|
+				|
+				|
+		=========
+	`
+
+	stages[5] = `
+			+---+
+			|   |
+			O   |
+				|
+				|
+				|
+		=========
+	`
+
+	stages[6] = `
+			+---+
+			|   |
+				|
+				|
+				|
+				|
+		=========
+	`
+
+	lifes = 6
+	word = strings.ToLower(randomWord())
+
+	logo.Print()
+	for lifes >= 0 {
+		screen.Clear()
+		screen.MoveTopLeft()
+
+		fmt.Printf(stages[lifes])
+		fmt.Println("Digit a letter: ")
+		fmt.Scanln(&input)
+		input = strings.ToLower(input)
+
+		if len(input) == 1 {
+
+			for pos, char := range word {
+				fmt.Printf("character %c starts at byte position %d\n", char, pos)
+			}
+
+			lifes--
+
+		} else {
+			if input == word {
+				win.Print()
+				break
+			} else {
+				lifes--
+			}
+		}
+
 	}
+
+	if lifes < 0 {
+		lost.Print()
+	}
+
 }
